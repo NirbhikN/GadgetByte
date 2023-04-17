0f1ea3fbe86d2c22c5a4c88e6e3b736dae1f1c89
NirbhikN <nyoupane.nirbhik@gmail.com>
2023-04-18 00:19:16 +0545

    created user, product and order model for mongo

---
+++ backend/models/productModel.js
@@ -0,0 +1,65 @@
+import mongoose, { mongo } from "mongoose";
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId, //* ObjectID refers another document in a different collection
+    required: true,
+    ref: "User", //* Reference of the model 'User' & adds relation between product and user
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  reviews: [reviewSchema],
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
