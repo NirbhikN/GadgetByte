dd181efbf79a935c4d7fefbfd86375d43cdeb370
NirbhikN <nyoupane.nirbhik@gmail.com>
2023-04-25 14:05:19 +0545

    user login reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,19 +9,30 @@ import {
 } from "./reducers/productReducers";
 
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
+//! Its key value is shown in redux devtool like productList, productDetails etc
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
+
+//* data in localstorage is added in 'actions' then added to initial here.
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+//* What should be in initital in localstorage
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 const middleware = [thunk];
 
