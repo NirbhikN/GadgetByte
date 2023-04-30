400ae8f427912e273450c226d5297564d506b578
NirbhikN <nyoupane.nirbhik@gmail.com>
2023-04-30 16:36:08 +0545

    Admin user list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
 
 const App = () => {
   return (
@@ -31,6 +32,7 @@ const App = () => {
             <Route path="/profile" element={<ProfileScreen />}></Route>
             <Route path="/product/:id" element={<ProductScreen />}></Route>
             <Route path="/cart/:id?" element={<CartScreen />}></Route>
+            <Route path="/admin/userlist" element={<UserListScreen />}></Route>
             <Route path="/" exact element={<HomeScreen />}></Route>
           </Routes>
         </Container>
